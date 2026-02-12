a60f4916b0b58b9a98cceb09e00b3b4146214833
Hadiqa Noor <151162691+HadiqaNoor@users.noreply.github.com>
2026-02-12 15:51:15 +0500

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import Attendance from "./pages/Attendance";
+import Students from "./pages/stu"; 
+import AttendanceReport from "./pages/AttendanceReport";
+import StudentDetail from "./pages/StudentDetail";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/attendance" element={<Attendance />} />
+        <Route path="/attendance-report" element={<AttendanceReport />} />
+        <Route path="/students/:id" element={<StudentDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
